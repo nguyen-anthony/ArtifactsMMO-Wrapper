1b5047d656257ef22acb6bb39b16a366c8edccfd
Veillax <126523099+Veillax@users.noreply.github.com>
2024-11-12 08:56:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="artifactsmmo_wrapper",
-    version="1.1.2",
+    version="1.1.3",
     author="Veillax",
     author_email="contact@veillax.com",
     description="A Python API Wrapper for ArtifactsMMO",

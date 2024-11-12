45417a1da514d110018ef230857a10604406e445
Veillax <126523099+Veillax@users.noreply.github.com>
2024-11-11 17:54:11 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="artifacts_mmo",
+    version="1.0.0",
+    author="Veillax",
+    author_email="contact@veillax.com",
+    description="A Python API Wrapper for ArtifactsMMO",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Veillax/ArtifactsMMO-S3-Wrapper", 
+    packages=find_packages(where="src"),         # Look for packages in 'src' if used
+    package_dir={"": "src"},                     # Maps root package to 'src'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+    install_requires=[
+        "requests"
+    ],
+)
